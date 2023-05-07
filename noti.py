a037fddae124ee8650da901f9c94f4a7dffadfae
Siyeon <siyeon9790@gmail.com>
2023-05-07 12:34:44 -0400

    first commit

---
+++ noti.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+import water
+if __name__ == '__main__':
+    while True:
+        notification.notify(
+            title = "Please Drink A Cup Of Water Now!!",
+            message ="Drinking Water Helps to Maintain the Balance of Body Fluids.",
+            app_icon='water.png',
+            timeout= 10
+            )
+        time.sleep(60*60*time_interval)
